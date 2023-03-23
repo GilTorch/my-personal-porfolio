758e7989896a658d6dc8e2d2e44138b3d16019b5
GilTorch <infotorch2014@gmail.com>
2023-03-23 14:16:47 -0400

    Send contact from the server

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,16 +1,91 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from 'nodemailer'
+import {google} from 'googleapis'
+
+// create OAuth2 client credentials
+const CLIENT_ID = process.env.CLIENT_ID 
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI = process.env.REDIRECT_URI
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
+oAuth2Client.setCredentials({
+    refresh_token: REFRESH_TOKEN
+})
+// replace with your refresh token
+oAuth2Client.setCredentials({
+  refresh_token: REFRESH_TOKEN,
+});
 
 type Data = {
-  name: string
+  success: boolean;
+  message: string;
 }
 
-export default function handler(
+type Email = {
+  email: string; 
+  name: string; 
+  message: string;
+}
+
+const sendMail = async ({ email, name, message }: Email) => {
+
+  const accessToken = await oAuth2Client.getAccessToken()
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: "infotorch2014@gmail.com", // replace with your Gmail email address
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+      accessToken: accessToken,
+      refreshToken: REFRESH_TOKEN
+    },
+  });
+
+  const mailOptions = {
+    from: `${name} <${email}>`,
+    to: "infotorch2014@gmail.com",
+    subject: "Message From Your Portfolio",
+    text: message,
+    // html: "",
+  }
+
+  // send mail with defined transport object
+  return transporter.sendMail(mailOptions,(error) => {
+    if(error){
+      console.log(error);
+    }else{
+      console.log("Message sent: %s ", info.messageId)
+    }
+
+  });
+}
+
+ const contactFormHandler = async (
   req: NextApiRequest,
   res: NextApiResponse<Data>
-) {
+ ) => {
 
-  console.log("Clearly this is working"+JSON.stringify(req.body))
+  if(req.method === "POST"){
+    const { name, message, email } = req.body
+    console.log(`REQ BODY :${JSON.stringify(req.body)}`)
+  
+    if(name && message && email ){
+      try {
+       await sendMail({ name, message, email})
+       res.status(200).json({ success: true, message: "Message sent successfully!" })
+      }catch(e){
+        res.status(500).json({ success: false, message: "Something unexpected happened while sending your message."})
+      }
+    }
+  }
 
-  res.status(200).json({ name: 'John Doe' })
+
+
 }
+
+export default contactFormHandler
+
