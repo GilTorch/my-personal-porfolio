dff2167a6f93e12c0666b118b47a088002ca206c
GilTorch <infotorch2014@gmail.com>
2024-10-10 23:49:27 -0400

    fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,109 +1,118 @@
-import { useEffect, useState } from 'react'
-import Link from 'next/link'
-import GilbertLogo from '../../public/svg/gilbert-logo.svg'
-import styles from '@/styles/Header.module.css'
-import { RefObject } from 'react'
-import { Button } from '@/components'
-import { Menu, Close } from '@mui/icons-material'
-
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import GilbertLogo from "../../public/svg/gilbert-logo.svg";
+import styles from "@/styles/Header.module.css";
+import { RefObject } from "react";
+import { Button } from "@/components";
+import { Menu, Close } from "@mui/icons-material";
 
 type Props = {
   sectionRefs: Array<RefObject<HTMLDivElement>>;
   onMobile: boolean;
   onBioLinkPressed: Function;
-}
+};
 
 type LinkData = {
   label: string;
-  sectionRef: RefObject<HTMLDivElement>
-}
+  sectionRef: RefObject<HTMLDivElement>;
+};
 
-const Header: React.FC<Props> = ({ sectionRefs, onMobile, onBioLinkPressed }) => {
-
-  const [navbarVisible, setNavbarVisible] = useState(true)
+const Header: React.FC<Props> = ({
+  sectionRefs,
+  onMobile,
+  onBioLinkPressed,
+}) => {
+  const [navbarVisible, setNavbarVisible] = useState(true);
 
   const links = [
     {
-      label: "BIO", 
+      label: "BIO",
       sectionRef: sectionRefs[0],
     },
     {
-      label: "EXPERTISE & SKILLS", 
+      label: "EXPERTISE & SKILLS",
       sectionRef: sectionRefs[1],
     },
     {
-      label: "PORTFOLIO", 
+      label: "PORTFOLIO",
       sectionRef: sectionRefs[2],
     },
     {
-      label: "CERTIFICATIONS", 
+      label: "CERTIFICATIONS",
       sectionRef: sectionRefs[3],
     },
     {
       label: "CONTACT",
-      sectionRef: sectionRefs[4]
-    }
-  ]
+      sectionRef: sectionRefs[4],
+    },
+  ];
 
   const onLinkPressed = (link: LinkData) => {
-    let yOffset = -325
-    if(link.label == "BIO"){
-        onBioLinkPressed()
+    let yOffset = -325;
+    if (link.label == "BIO") {
+      onBioLinkPressed();
     }
 
-    const sectionRef = link.sectionRef
+    const sectionRef = link.sectionRef;
 
-    if(sectionRef && sectionRef.current){
-      const y =  sectionRef.current.getBoundingClientRect().top + window.pageYOffset + yOffset;
-      window.scrollTo({top: y, behavior: "smooth"})
+    if (sectionRef && sectionRef.current) {
+      const y =
+        sectionRef.current.getBoundingClientRect().top +
+        window.pageYOffset +
+        yOffset;
+      window.scrollTo({ top: y, behavior: "smooth" });
     }
+  };
 
-  }
+  useEffect(() => {
+    setNavbarVisible(!onMobile);
+  }, [onMobile]);
 
-  useEffect(() =>{
-      setNavbarVisible(!onMobile)
-},[onMobile])
-
-  
-  return(
+  return (
     <header>
-      {
-        onMobile && (
-          <div onClick={() => {
-            setNavbarVisible((visible) => !visible)
-          }} 
+      {onMobile && (
+        <div
+          onClick={() => {
+            setNavbarVisible((visible) => !visible);
+          }}
           className={`${styles.closeButtonContainer}`}
-          >
-            {navbarVisible ? (
-              <Close htmlColor="#EFC900"/>
-            ):(
-              <Menu htmlColor='#EFC900' />
-            )}
-          </div>
-        )
-      }
-    <nav style={{display: navbarVisible ? "block" : "none"}} className={`${styles.nav} slide-up `}>
-      <ul className={styles.links}>
-        <Link href="/" className={styles.logo}>
-          <GilbertLogo />
-        </Link>
-        {links.map((link,index) => (
-          <li key={index} className={styles.link} onClick={() => onLinkPressed(link)}>
-            {link.label}
-          </li>
-        ))}
-        <li className={styles.resumeButtonLinkContainer}>
-          <Link target="_blank" href="https://drive.google.com/file/d/1bHK7xjj9_oZpWoI1kta_4LiHRwTv4Qts/view?usp=share_link">
-           <Button>
-             RESUME
-           </Button>
+        >
+          {navbarVisible ? (
+            <Close htmlColor="#EFC900" />
+          ) : (
+            <Menu htmlColor="#EFC900" />
+          )}
+        </div>
+      )}
+      <nav
+        style={{ display: navbarVisible ? "block" : "none" }}
+        className={`${styles.nav} slide-up `}
+      >
+        <ul className={styles.links}>
+          <Link href="/" className={styles.logo}>
+            <GilbertLogo />
           </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
-}
+          {links.map((link, index) => (
+            <li
+              key={index}
+              className={styles.link}
+              onClick={() => onLinkPressed(link)}
+            >
+              {link.label}
+            </li>
+          ))}
+          <li className={styles.resumeButtonLinkContainer}>
+            <Link
+              target="_blank"
+              href="https://drive.google.com/file/d/1bHK7xjj9_oZpWoI1kta_4LiHRwTv4Qts/view?usp=share_link"
+            >
+              <Button>RESUME</Button>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
-
+export default Header;
